d741d23cfa6fbce1c83311e4f74231208a6f3a5c
Steve <stwhitejr@gmail.com>
2018-05-14 19:33:13 -0400

    removing config

--- code/config/config.php
+++
@@ -1,7 +0,0 @@
-<?
-	$conn = new mysqli('db558054145.db.1and1.com', 'dbo558054145', 'lesserEv!l123', 'db558054145');
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-?>
